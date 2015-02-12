7bda1f1ffe6b5c3fc7962211c4f716a28e74c127
Doug Nottingham <Lownslow200sx@gmail.com>
2015-02-11 22:34:43 -0500

    Fuck It

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,3 @@
-#
-# Copyright (C) 2015 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # CPU
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
@@ -60,11 +44,13 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
+
 # TWRP
+RECOVERY_VARIANT := twrp
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-
 DEVICE_RESOLUTION := 1440x2560
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_NO_USB_STORAGE := true
@@ -72,14 +58,5 @@ TW_INCLUDE_L_CRYPTO := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_REAL_SDCARD := true
-
-# MultiROM
-MR_INPUT_TYPE := type_b
-MR_INIT_DEVICES := device/lge/vs985/mr_init_devices.c
-MR_DPI := xhdpi
-MR_DPI_FONT := 230
-MR_FSTAB := device/lge/vs985/twrp.fstab
-MR_KEXEC_MEM_MIN := 0x20000000
-MR_KEXEC_DTB := true
-MR_USE_MROM_FSTAB := true
+TARGET_RECOVERY_INITRC := device/lge/vs985/init.rc
 
